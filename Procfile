1859a485436c76a0ef410a4f304333cf33ce7a48
Chris C Cerami <chrisccerami@gmail.com>
2015-01-12 23:49:55 -0500

    Added procfile command to seed database with new images

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: rake db:seed
--- scraper.rb
+++ scraper.rb
@@ -5,14 +5,14 @@ require 'open-uri'
 # grabs the HTML from the main page of the curiosity rover image gallery
 doc = Nokogiri::HTML(open("http://mars.jpl.nasa.gov/msl/multimedia/raw/"))
 # collects link suffixes to all pages for each martian solar cycle from each camera
-results = doc.css("div.image_list a").map { |link|  link['href']}
+@results = doc.css("div.image_list a").map { |link|  link['href']}
 base_url = "http://mars.jpl.nasa.gov/msl/multimedia/raw/"
 # attaches link suffixes to the base_url
-results.map! { |suffix| base_url + suffix }
+@results.map! { |suffix| base_url + suffix }
 
-images = Array.new
+@images = Array.new
 def scrape_images
-  results.each do |url|
+  @results.each do |url|
     image_hash = Hash.new
     image_page = Nokogiri::HTML(open(url))
     image_array = image_page.css("div.RawImageCaption div.RawImageUTC a").map { |link| link["href"] }
@@ -21,10 +21,10 @@ def scrape_images
         image_hash[:img_src] = image
         image_hash[:sol] = url.scan(/(?<==)\d+/).first
         image_hash[:camera] = url.scan(/(?<=camera=)\w+/).first
-        images << image_hash
+        @images << image_hash
+        print "."
       end
     end
   end
-  images
+  @images
 end
-
