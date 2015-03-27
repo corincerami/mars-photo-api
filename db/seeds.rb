b2e7236f49055942b725c3b52c4bb2cb47777fae
Chris C Cerami <chrisccerami@gmail.com>
2015-03-27 11:50:12 -0400

    Fixes to scraper and seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ require './scraper'
 
 images = scrape_images
 
-image.each do |image|
-  i = Image.find_or_create_by(sol: image["sol"], camera: image["camera"], img_src: image["img_src"])
-  puts "Image #{i.id}"
+images.each do |image|
+  p = Photo.find_or_create_by(sol: image[:sol], camera: image[:camera], img_src: image[:img_src])
+  puts "Image #{p.id}"
 end
