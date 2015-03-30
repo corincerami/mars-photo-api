d81164833621e5a49144445aacb9bad5a0607140
Chris C Cerami <chrisccerami@gmail.com>
2015-03-30 02:20:34 -0400

    Allow a phots endpoint that returns all images from a particular sol across all cameras

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,6 +2,10 @@ class Photo < ActiveRecord::Base
   validates :img_src, uniqueness: true
 
   def self.search(sol, camera)
-    where(sol: sol, camera: camera)
+    if sol && camera
+      where(sol: sol, camera: camera)
+    elsif sol
+      where(sol: sol)
+    end
   end
 end
