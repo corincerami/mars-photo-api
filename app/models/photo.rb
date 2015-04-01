937a534f6c37ff9fa26dce8e3bb53abf490fac40
Chris C Cerami <chrisccerami@gmail.com>
2015-04-01 00:22:12 -0400

    Add earth_date to photo model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,11 +1,19 @@
 class Photo < ActiveRecord::Base
   validates :img_src, uniqueness: true
 
+  LANDING_DATE = DateTime.new(2012,8,6,5,17,57)
+  SOL_IN_SECONDS = 88775.244
+
   def self.search(sol, camera)
     if sol && camera
       where(sol: sol, camera: camera)
     elsif sol
       where(sol: sol)
     end
+  end
+
+  def earth_date
+    date = LANDING_DATE + (sol.to_i * SOL_IN_SECONDS).seconds
+    date.strftime("%b %e, %Y")
   end
 end
