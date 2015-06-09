00f5e902bf76cc94434eeec40c94d2ed8a145830
Chris C Cerami <chrisccerami@gmail.com>
2015-06-09 19:35:03 -0400

    Create versioned API namespaced controller

--- app/controllers/api/v1/photos_controller.rb
+++ app/controllers/api/v1/photos_controller.rb
@@ -1,9 +1,11 @@
-class PhotosController < ApplicationController
+class Api::V1::PhotosController < ApplicationController
   def show
     @photo = Photo.find(params[:id])
+    render json: @photo
   end
 
   def index
     @photos = Photo.search(params[:sol], params[:camera])
+    render json: @photos
   end
 end
