b0ca2b8a6c4bd3ffa79f8919edff4fd311d254dc
Chris C Cerami <chrisccerami@gmail.com>
2015-06-10 01:01:54 -0400

    Add earth_date as a column on the photos table

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,6 @@
 class Photo < ActiveRecord::Base
+  after_create :set_earth_date
+
   validates :img_src, uniqueness: true
 
   LANDING_DATE = DateTime.new(2012,8,6,5,17,57)
@@ -12,8 +14,15 @@ class Photo < ActiveRecord::Base
     end
   end
 
-  def earth_date
-    date = LANDING_DATE + (sol.to_i * SOL_IN_SECONDS).seconds
-    date.strftime("%b %e, %Y")
+  def formatted_earth_date
+    self.earth_date.strftime("%b %e, %Y")
+  end
+
+  def calculate_earth_date
+    LANDING_DATE + (sol.to_i * SOL_IN_SECONDS).seconds
+  end
+
+  def set_earth_date
+    self.update(earth_date: self.calculate_earth_date)
   end
 end
