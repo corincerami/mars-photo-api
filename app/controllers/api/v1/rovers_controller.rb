320c1f4486d9df6748834e0fef3c74be774412d3
Chris C Cerami <chrisccerami@gmail.com>
2015-06-21 10:25:40 -0400

    Add api show path to Rover

--- app/controllers/api/v1/rovers_controller.rb
+++ app/controllers/api/v1/rovers_controller.rb
@@ -3,4 +3,9 @@ class Api::V1::RoversController < ApplicationController
     @rovers = Rover.all
     render json: @rovers
   end
+
+  def show
+    @rover = Rover.find_by(name: params[:id].capitalize)
+    render json: @rover
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :rovers, only: [:index] do
+      resources :rovers, only: [:show, :index] do
         resources :photos, only: [:show, :index]
       end
     end
