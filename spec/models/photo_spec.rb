38fbf2193f9dd27e36ea51b3b49a8d9df53bc9c6
Chris C Cerami <chrisccerami@gmail.com>
2015-06-28 02:17:55 -0400

    Add test for Photo#search

--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -12,4 +12,44 @@ RSpec.describe Photo, type: :model do
     it { should belong_to :rover }
     it { should belong_to :camera }
   end
+
+  describe ".search" do
+    context "with sol query" do
+      it "returns matching photos" do
+        @photo = FactoryGirl.create(:photo)
+        params = { sol: 829, rover: @photo.rover.name }
+
+        expect(Photo.search(params, @photo.rover.name)).to include @photo
+      end
+    end
+
+    context "with sol and camera query" do
+      it "returns matching photos" do
+        @photo = FactoryGirl.create(:photo)
+        @photo.camera.update(rover: @photo.rover)
+        params = { sol: 829, rover: @photo.rover.name, camera: @photo.camera.name }
+
+        expect(Photo.search(params, @photo.rover.name)).to include @photo
+      end
+    end
+
+    context "with Earth date query" do
+      it "returns matching photos" do
+        @photo = FactoryGirl.create(:photo)
+        params = { earth_date: "2014-12-05" }
+
+        expect(Photo.search(params, @photo.rover.name)).to include @photo
+      end
+    end
+
+    context "with Earth date and camera query" do
+      it "returns matching photos" do
+        @photo = FactoryGirl.create(:photo)
+        @photo.camera.update(rover: @photo.rover)
+        params = { earth_date: "2014-12-05", camera: @photo.camera.name }
+
+        expect(Photo.search(params, @photo.rover.name)).to include @photo
+      end
+    end
+  end
 end
