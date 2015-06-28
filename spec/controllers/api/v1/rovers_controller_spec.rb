a10ec6d40626bdafed8cbf795bbc278b00647edc
Chris C Cerami <chrisccerami@gmail.com>
2015-06-28 01:42:27 -0400

    Add tests for Api::V1::RoversController show path

--- spec/controllers/api/v1/rovers_controller_spec.rb
+++ spec/controllers/api/v1/rovers_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe Api::V1::RoversController do
   describe "GET 'index'" do
 
-    context 'with no query parameters' do
+    context "with no query parameters" do
 
       before(:each) do
         @rover = FactoryGirl.create(:rover)
@@ -14,8 +14,25 @@ describe Api::V1::RoversController do
         expect(response.status).to eq 200
       end
 
-      it 'renders rovers json' do
+      it "renders rovers json" do
         expect(json["rovers"].length).to eq 1
+      end
+    end
+  end
+
+  describe "GET 'show'" do
+    context "with a valid rover name" do
+      before(:each) do
+        @rover = FactoryGirl.create(:rover)
+        get :show, { id: @rover.name }
+      end
+
+      it "returns http 200 success" do
+        expect(response.status).to eq 200
+      end
+
+      it "renders the proper rover json" do
+        expect(json["rover"]["name"]).to eq @rover.name
       end
     end
   end
