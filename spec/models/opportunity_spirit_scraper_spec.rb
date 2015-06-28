f0a4a1feb91246e24bca5bdca4c5e9e597e129d4
Chris C Cerami <chrisccerami@gmail.com>
2015-06-28 19:56:43 -0400

    Add test for OpportunitySpiritScraper sol_paths

--- spec/models/opportunity_spirit_scraper_spec.rb
+++ spec/models/opportunity_spirit_scraper_spec.rb
@@ -9,4 +9,14 @@ RSpec.describe OpportunitySpiritScraper, type: :model do
       expect(scraper.main_page.title).to eq "Mars Exploration Rover Mission: Multimedia: All Raw Images: Opportunity"
     end
   end
+
+  describe ".sol_paths" do
+    it "should return paths for individual sol pages" do
+      opp = FactoryGirl.create(:rover, name: "Opportunity")
+      scraper = OpportunitySpiritScraper.new("Opportunity")
+
+      OpportunitySpiritScraper::SOL_SELECT_CSS_PATHS = ["select[id^=Engineering_Cameras_Entry]"]
+      expect(scraper.sol_paths).to eq ["opportunity_e001.html"]
+    end
+  end
 end
