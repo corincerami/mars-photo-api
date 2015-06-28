60e28c43d97f12eb44c50055901e2adac115b87f
Chris C Cerami <chrisccerami@gmail.com>
2015-06-28 19:52:17 -0400

    Add test for OpportunitySpiritScraper main_page

--- spec/models/opportunity_spirit_scraper_spec.rb
+++ spec/models/opportunity_spirit_scraper_spec.rb
@@ -1,4 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe OpportunitySpiritScraper, type: :model do
+  describe ".main_page" do
+    it "should return a Nokogiri page" do
+      opp = FactoryGirl.create(:rover, name: "Opportunity")
+      scraper = OpportunitySpiritScraper.new("Opportunity")
+
+      expect(scraper.main_page.title).to eq "Mars Exploration Rover Mission: Multimedia: All Raw Images: Opportunity"
+    end
+  end
 end
