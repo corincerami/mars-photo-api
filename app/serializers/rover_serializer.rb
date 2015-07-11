0ebb64308a0b41f2a7b3809aa643eeda726c8773
Chris C Cerami <chrisccerami@gmail.com>
2015-07-11 17:23:32 -0400

    Add ID attribute to rover_serializer

--- app/serializers/rover_serializer.rb
+++ app/serializers/rover_serializer.rb
@@ -1,5 +1,5 @@
 class RoverSerializer < ActiveModel::Serializer
-  attributes :name, :landing_date, :max_sol, :max_date, :total_photos
+  attributes :id, :name, :landing_date, :max_sol, :max_date, :total_photos
 
   has_many :cameras
 end
