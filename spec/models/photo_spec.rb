d6e82af0794a55e76aa9597da4afe7ef2a10ac22
Chris C Cerami <chrisccerami@gmail.com>
2015-12-06 01:35:15 -0500

    Raise exception if camera not found during scrape

--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -1,18 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Photo, type: :model do
-  describe "attributes" do
-    it { should respond_to :img_src }
-    it { should respond_to :camera }
-    it { should respond_to :sol }
-    it { should respond_to :earth_date }
-  end
-
-  describe "associations" do
-    it { should belong_to :rover }
-    it { should belong_to :camera }
-  end
-
   describe ".search" do
     context "with sol query" do
       it "returns matching photos" do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,6 +19,7 @@ CodeClimate::TestReporter.start
 RSpec.configure do |config|
   require "json"
   require "support/request_helpers"
+  require "factory_girl"
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
@@ -33,6 +34,7 @@ RSpec.configure do |config|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
   config.include Requests::JsonHelpers, type: :controller
+  config.include FactoryGirl::Syntax::Methods
 
   # rspec-mocks config goes here. You can use an alternate test double
   # library (such as bogus or mocha) by changing the `mock_with` option here.
