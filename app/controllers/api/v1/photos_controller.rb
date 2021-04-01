4bb3f58b7d251a7c2dc0afa8ee26253171dee039
Mathilda Grace <63630918+mtilda@users.noreply.github.com>
2021-04-01 11:51:50 -0400

    Permit size

--- app/controllers/api/v1/photos_controller.rb
+++ app/controllers/api/v1/photos_controller.rb
@@ -1,13 +1,33 @@
 class Api::V1::PhotosController < ApplicationController
+  before_action :photo_params
+
   def show
-    photo = Photo.find params[:id]
-    render json: photo, serializer: PhotoSerializer, root: :photo
+    photo = Photo.find photo_params[:id]
+    photo = helpers.resize_photo photo, photo_params
+
+    if !photo.nil?
+      render json: photo, serializer: PhotoSerializer, root: :photo
+    else
+      render json: {
+        errors: "Invalid size parameter '#{photo_params[:size]}' for '#{photo_params[:rover_id].titleize}' photos"
+      }, status: :bad_request
+    end
   end
 
   def index
-    rover = Rover.find_by name: params[:rover_id].titleize
+    rover = Rover.find_by name: @params[:rover_id].titleize
+
     if rover
-      render json: search_photos(rover), each_serializer: PhotoSerializer, root: :photos
+      photos = helpers.search_photos rover, photo_params
+      photos = helpers.resize_photos photos, photo_params
+
+      if !photos.nil?
+        render json: photos, each_serializer: PhotoSerializer, root: :photos
+      else
+        render json: {
+          errors: "Invalid size parameter '#{photo_params[:size]}' for '#{rover.titleize}' photos"
+        }, status: :bad_request
+      end
     else
       render json: { errors: "Invalid Rover Name" }, status: :bad_request
     end
@@ -16,14 +36,6 @@ class Api::V1::PhotosController < ApplicationController
   private
 
   def photo_params
-    params.permit :sol, :camera, :earth_date, :rover_id
-  end
-
-  def search_photos(rover)
-    photos = rover.photos.order(:camera_id, :id).search photo_params, rover
-    if params[:page]
-      photos = photos.page(params[:page]).per params[:per_page]
-    end
-    photos
+    @params = params.permit :id, :rover_id, :sol, :camera, :earth_date, :size, :page, :per_page
   end
 end
