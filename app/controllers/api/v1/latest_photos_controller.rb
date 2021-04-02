885720017938a90312114784c314e5a6c8065855
Mathilda Grace <63630918+mtilda@users.noreply.github.com>
2021-04-01 20:38:42 -0400

    Add photo resizing to latest_photos_controller

--- app/controllers/api/v1/latest_photos_controller.rb
+++ app/controllers/api/v1/latest_photos_controller.rb
@@ -1,8 +1,17 @@
 class Api::V1::LatestPhotosController < ApplicationController
+  include PhotoHelper
+
   def index
     @rover = Rover.find_by name: params[:rover_id].titleize
     if @rover
-      render json: search_photos, each_serializer: PhotoSerializer, root: :latest_photos
+      photos = search_photos(@rover)
+      error = resize_photos photos, params
+
+      if error.nil?
+        render json: photos, each_serializer: PhotoSerializer, root: :latest_photos
+      else
+        render json: { errors: error }, status: :bad_request
+      end
     else
       render json: { errors: "Invalid Rover Name" }, status: :bad_request
     end
@@ -14,11 +23,12 @@ class Api::V1::LatestPhotosController < ApplicationController
     params.permit(:camera, :earth_date, :rover_id).merge(sol: @rover.photos.maximum(:sol))
   end
 
-  def search_photos
-    photos = @rover.photos.order(:camera_id, :id).search photo_params, @rover
+  def search_photos rover
+    photos = rover.photos.order(:camera_id, :id).search photo_params, rover
     if params[:page]
       photos = photos.page(params[:page]).per params[:per_page]
     end
+
     photos
   end
 end
