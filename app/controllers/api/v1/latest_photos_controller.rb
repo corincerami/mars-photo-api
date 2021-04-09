e5788744424f95bcf93ccf182571f8b6090f9283
Mathilda Grace <63630918+mtilda@users.noreply.github.com>
2021-04-09 00:19:38 -0400

    Fix params name conflict

--- app/controllers/api/v1/latest_photos_controller.rb
+++ app/controllers/api/v1/latest_photos_controller.rb
@@ -3,16 +3,16 @@ class Api::V1::LatestPhotosController < ApplicationController
     @rover = Rover.find_by name: params[:rover_id].titleize
 
     if @rover then
-      params = params
+      validated_params = params
         .permit(:rover_id, :camera, :earth_date, :size, :page, :per_page)
         .merge(sol: @rover.photos.maximum(:sol))
-      photos = rover.photos.search params, @rover
-      photos = helpers.resize_photos photos, params
+      photos = @rover.photos.search validated_params, @rover
+      photos = helpers.resize_photos photos, validated_params
 
       if photos != 'size error'
         render json: photos, each_serializer: PhotoSerializer, root: :latest_photos
       else
-        render json: { errors: "Invalid size parameter '#{photo_params[:size]}' for #{@rover.name.titleize}" }, status: :bad_request
+        render json: { errors: "Invalid size parameter '#{validated_params[:size]}' for #{@rover.name.titleize}" }, status: :bad_request
       end
     else
       render json: { errors: "Invalid Rover Name" }, status: :bad_request
