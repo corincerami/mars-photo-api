b6182fb6f345e2261b974d5988a45c3b62a6547b
Mathilda Grace <63630918+mtilda@users.noreply.github.com>
2021-04-10 19:23:56 -0400

    Change instance variable rover to local variable

--- app/controllers/api/v1/latest_photos_controller.rb
+++ app/controllers/api/v1/latest_photos_controller.rb
@@ -1,18 +1,18 @@
 class Api::V1::LatestPhotosController < ApplicationController
   def index
-    @rover = Rover.find_by name: params[:rover_id].titleize
+    rover = Rover.find_by name: params[:rover_id].titleize
 
-    if @rover then
+    if rover then
       validated_params = params
         .permit(:rover_id, :camera, :earth_date, :size, :page, :per_page)
-        .merge(sol: @rover.photos.maximum(:sol))
-      photos = @rover.photos.search validated_params, @rover
+        .merge(sol: rover.photos.maximum(:sol))
+      photos = rover.photos.search validated_params, rover
       photos = helpers.resize_photos photos, validated_params
 
       if photos != 'size error'
         render json: photos, each_serializer: PhotoSerializer, root: :latest_photos
       else
-        render json: { errors: "Invalid size parameter '#{validated_params[:size]}' for #{@rover.name.titleize}" }, status: :bad_request
+        render json: { errors: "Invalid size parameter '#{validated_params[:size]}' for #{rover.name.titleize}" }, status: :bad_request
       end
     else
       render json: { errors: "Invalid Rover Name" }, status: :bad_request
