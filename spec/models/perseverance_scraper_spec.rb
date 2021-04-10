29fbd076f7ab117f60e9400d83f2e7349d0af270
mxkay <codingwithkay@gmail.com>
2021-04-10 19:11:49 -0400

    Add test for invalid camera name

--- spec/models/perseverance_scraper_spec.rb
+++ spec/models/perseverance_scraper_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe PerseveranceScraper, type: :model do
   let!(:perseverance) { create(:rover, name: "Perseverance", landing_date: Date.new(2021, 2, 18), launch_date: Date.new(2020, 7, 30), status: "active") }
   let(:scraper) { PerseveranceScraper.new }
+
   describe "#rover" do
     it "should be Perseverance" do
       expect(scraper.rover).to eq perseverance
@@ -39,6 +40,22 @@ RSpec.describe PerseveranceScraper, type: :model do
       allow(scraper).to receive(:collect_links).and_return ["https://mars.nasa.gov/rss/api/?feed=raw_images&category=mars2020&feedtype=json&sol=1"]
 
       expect{ scraper.scrape }.to change { Photo.count }.by(34)
+    end
+
+    context "finds an invalid camera name" do
+      before(:each) do
+        allow($stdout).to receive(:write) # stub stdout
+        allow(scraper).to receive(:collect_links).and_return ["https://mars.nasa.gov/rss/api/?feed=raw_images&category=mars2020&feedtype=json&sol=1"]
+        allow(scraper).to receive(:camera_from_json).and_return('NOT_A_CAMERA')
+      end
+
+      it "should not create any photos" do
+        expect { scraper.scrape }.to change { Photo.count }.by(0)
+      end
+
+      it "should print a warning" do
+        expect { scraper.scrape }.to output(/WARNING: Camera not found. Name: NOT_A_CAMERA/).to_stdout
+      end
     end
   end
 end
